f98228644689bbf57076dd712b7e32cf26fbfe38
majafra <bartlomiej.wit@gmail.com>
2020-10-26 23:26:14 +0100

    swagger

--- src/main/java/com/crud/tasks/config/CoreConfiguration.java
+++ src/main/java/com/crud/tasks/config/CoreConfiguration.java
@@ -21,7 +21,7 @@ public class CoreConfiguration implements WebMvcConfigurer {
     }
     @Bean
     public Docket api()
-    { return new Docket(DocumentationType.SWAGGER_2) .select() .apis(RequestHandlerSelectors.any()) .paths(PathSelectors.any()) .build(); }
+    { return new Docket(DocumentationType.SWAGGER_2) .select() .apis(RequestHandlerSelectors.basePackage("com.crud.tasks.controller")) .paths(PathSelectors.any()) .build(); }
 
     @Override
     public void addResourceHandlers(final ResourceHandlerRegistry registry) {
